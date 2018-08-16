c7cf15718029da555e6421acd7971bb8de0c6071
aaranderson <andaa635@student.otago.ac.nz>
2018-08-16 15:54:12 +1200

    Initial Commit

---
+++ Be nice to William/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Be nice to William
+//
+//  Created by Aaron Anderson on 8/16/18.
+//  Copyright © 2018 Aaron Anderson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
